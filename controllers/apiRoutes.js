0301c377216700731eb6d8d67430cc22fc3bbc20
Derek DeLizo <delizoderek@gmail.com>
2021-08-16 22:42:07 -0700

    Created api routes and connected the router

---
+++ controllers/apiRoutes.js
@@ -0,0 +1,26 @@
+const apiRouter = require('express').Router();
+const db = require('../models');
+
+// Get last workout
+apiRouter.get('/',(req,res) => {
+    res.send('Hello from your server!');
+});
+
+// Get workouts in range
+apiRouter.get('/',(req,res) => {
+    res.send('Hello from your server!');
+});
+
+// Add exercise to current workout
+apiRouter.put('/',(req,res) => {
+    console.log('req.baseurl');
+    res.send(`${req.method} request received`);
+});
+
+// Create new workout
+apiRouter.post('/',(req,res) => {
+    console.log('req.baseurl');
+    res.send(`${req.method} request received`);
+});
+
+module.exports = apiRouter;
